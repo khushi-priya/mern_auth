22d9a9a49900674251a58a54aa21f74b97f2f821
khushi-priya <khushipriya056@gmail.com>
2025-01-30 00:50:52 +0530

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,8 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import  Header  from "./components/Header"
-
-
+import {Link} from 'react-router-dom';
+import "./App.css";
 
 export default function App() {
   return (
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
 
-const SignUp = () => {
+export default function SignUp() {
+  
   return (
-    <div>SignUp</div>
+    
+    <div className="p-3 max-w-lg mx-auto bg-black mt-12">
+      
+      <h1 className="text-3xl text-center font-semibolded my-7 text-white ">Sign Up</h1>
+      
+      <form className='flex flex-col gap-4'>
+
+        <input type="text" placeholder="username" id='username' 
+        className='bg-gray-300 p-3 rounded-lg' />
+        
+        <input type="email" placeholder="email" id='email' 
+        className='bg-gray-300 p-3 rounded-lg' />
+
+        <input type="password" placeholder="password" id='password' 
+        className='bg-gray-300 p-3 rounded-lg' />
+
+        <button className='bg-slate-700 p-3 disabled:opacity-80 hover:opacity-95 
+        uppercase rounded-lg text-red-500'>Sign Up</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p className='text-white'>Have an account?</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-600'>Sign in</span>
+        </Link>
+        
+      </div>
+    </div>
+    
   )
 }
 
-export default SignUp
