6c2fbfecd0d26fe7abf0b02933178f9cb3c449d5
khushi-priya <khushipriya056@gmail.com>
2025-01-29 19:45:02 +0530

    create user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,6 @@ const userSchema=new mongoose.Schema({
   
 },{timestamps:true});
 
-const User=mongoose.model('User',userShema);
+const User=mongoose.model('User',userSchema);
 
 export default User;
